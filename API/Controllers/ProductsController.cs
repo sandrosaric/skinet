93c7d4ab82760e5194707d788672b0854af08004
sandrosaric <sandro.saric@gmail.com>
2022-07-04 16:03:52 +0200

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        
+
+    }
+}
