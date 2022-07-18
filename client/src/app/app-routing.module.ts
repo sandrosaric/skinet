f17f54f52df47ebd5c71bd1cd364c7343bc99bee
sandrosaric <sandro.saric@gmail.com>
2022-07-18 12:40:34 +0200

    End of section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
@@ -8,6 +11,18 @@ const routes: Routes = [
   {
     path:"",
     component:HomeComponent
+  },
+  {
+    path:"test-errors",
+    component:TestErrorComponent
+  },
+  {
+    path:"server-error",
+    component:ServerErrorComponent
+  },
+  {
+    path:"not-found",
+    component:NotFoundComponent
   },
   {
     path:"shop",
