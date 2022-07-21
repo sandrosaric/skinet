f82efb27f1bad2a828a62347890196e985d853d9
sandrosaric <sandro.saric@gmail.com>
2022-07-21 11:14:52 +0200

    End of section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
 
@@ -13,10 +14,18 @@ export class AppComponent implements OnInit{
   title = 'Skinet';
 
 
-  constructor(){}
+  constructor(private basketService:BasketService){}
 
   ngOnInit(): void {
-
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId){
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log("initialised basket");
+      },
+      error => {
+        console.log(error);
+      })
+    }
   }
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error-interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { BasketModule } from './basket/basket.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
     HttpClientModule,
     CoreModule,
     HomeModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    BasketModule
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS, useClass:ErrorInterceptor,multi:true},
